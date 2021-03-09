cb0413dfa20208e34c8f881d9ad81dcea1a3163a
Corey Smith <CSSmith1997@gmail.com>
2021-03-08 21:48:28 -0700

    Updated server.js

--- routes/index.js
+++ routes/index.js
@@ -19,5 +19,7 @@ router.use('/mealplan', mealplanRoutes);
 // router.get('/user', (req, res) => {
 //     res.sendFile(path.join(__dirnam, '../weight-loss-app/public/index.html'))
 // });
-
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../weight-loss-app/build/index.html"));
+  });
 module.exports = router;
