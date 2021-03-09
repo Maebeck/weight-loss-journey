91965afa8ab8c106f15352fd5123de6c60e5375a
Corey Smith <CSSmith1997@gmail.com>
2021-03-08 23:46:12 -0700

    aoijg

--- routes/userRoutes/index.js
+++ routes/userRoutes/index.js
@@ -1,13 +1,46 @@
 const router = require('express').Router();
 let User = require ('../../models/usermodel');
 
-router.route('/user').get((req, res) => {
-    User.find()
+// Get All Users
+router.route('/users').get((req, res) => {
+    User.all
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
+// CRUD
+
+// Create User
+router.route('/users').post((req, res) => {
+    const username = req.body.username;
+
+    const newUser = new User({username});
+
+    newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Read User
+router.route('/users/{:id}').get((req, res) => {
+    User.find(id)
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Update User
+router.route('/users/{:id}').patch((req, res) => {
+    const username = req.body.username;
+
+    const newUser = new User({username});
+
+    newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Delete User
+router.route('/users/{:id}').delete((req, res) => {
     const username = req.body.username;
 
     const newUser = new User({username});
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3001;
-const routes = require('./routes');
+const routes = require('./routes/index');
 app.use(cors());
 app.use(express.json());
 
