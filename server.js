fe1fbe11251682f3809f8f551e5ebfe660f67a6e
Corey Smith <CSSmith1997@gmail.com>
2021-03-10 15:38:04 -0700

    more testing for bug fixes

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV === "production") {
     app.use(express.static("weight-loss-app/build"))
     // app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "weight-loss-app", "build", "index.html")))
   }
-app.use(routes);
+// app.use(routes);
 
 
 app.listen(port, () => {
