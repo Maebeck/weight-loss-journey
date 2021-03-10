221fcb4fb6264b864f065ad4ac658f19b06427fd
Corey Smith <CSSmith1997@gmail.com>
2021-03-10 16:15:57 -0700

    testing routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const journalRoutes = require('./journalRoutes');
-const mealplanRoutes = require('./mealplanRoutes');
+// const mealplanRoutes = require('./mealplanRoutes');
 const path = require('path');
 
 
@@ -8,7 +8,7 @@ const path = require('path');
 
 
 router.use('/journal', journalRoutes);
-router.use('/MealPlan', mealplanRoutes);
+// router.use('/MealPlan', mealplanRoutes);
 
 
 // // router.get('/journal', (req, res) => {
