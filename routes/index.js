a8fd07541610b3a2affc3eb01db6e43127d3e3d0
Corey Smith <CSSmith1997@gmail.com>
2021-03-10 16:00:09 -0700

    testing routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-// const router = require('express').Router();
-// const journalRoutes = require('./journalRoutes');
-// const mealplanRoutes = require('./mealplanRoutes');
-// const path = require('path');
+const router = require('express').Router();
+const journalRoutes = require('./journalRoutes');
+const mealplanRoutes = require('./mealplanRoutes');
+const path = require('path');
 
 
 // // const url = 'mongodb://localhost:3001/test';
 
 
-// router.use('/journal', journalRoutes);
-// router.use('/mealplan', mealplanRoutes);
+router.use('/journal', journalRoutes);
+router.use('/MealPlan', mealplanRoutes);
 
 
 // // router.get('/journal', (req, res) => {
@@ -20,7 +20,7 @@
 // // router.get('/user', (req, res) => {
 // //     res.sendFile(path.join(__dirnam, '../weight-loss-app/public/index.html'))
 // // });
-// router.use(function(req, res) {
-//     res.sendFile(path.join(__dirname, "../weight-loss-app/build/index.html"));
-//   });
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../weight-loss-app/build/index.html"));
+  });
 // module.exports = router;
